973e5e43366fb484e778caf1f7ae675efac15c98
Nicolai Ommer <nicolai.ommer@gmail.com>
2012-01-06 18:34:41 +0100

    Corrected a problem with some files

--- debian/t10-keyboard.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -jar /usr/share/t10-keyboard/t10-keyboard-v1.2-3-ge4ebb11.jar $@
--- prepackaging.sh
+++ prepackaging.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 echo "update debian/t10-keyboard.install:"
-echo "`cat debian/t10-keyboard.install | grep -v .jar`" > debian/t10-keyboard.install
+#echo "`cat debian/t10-keyboard.install | grep -v .jar`" > debian/t10-keyboard.install
+echo "debian/t10-keyboard.sh usr/share/t10-keyboard/
+debian/t10-keyboard.desktop /usr/share/applications
+debian/t10-keyboard_icon.png usr/share/t10-keyboard" > debian/t10-keyboard.install
+
 version="`cat src/res/version`"
 if [ -z "$version" ]; then
   echo "Error: Version empty"
@@ -15,6 +19,7 @@ fi
 echo "build/$filename usr/share/t10-keyboard" >> debian/t10-keyboard.install
 cat debian/t10-keyboard.install
 
+echo
 echo "update debian/t10-keyboard.sh:"
 echo "#!/bin/sh" > debian/t10-keyboard.sh
 echo "java -jar /usr/share/t10-keyboard/$filename \$@" >> debian/t10-keyboard.sh
